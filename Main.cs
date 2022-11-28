61da74f9da78b1e70e9e5b4be5a0737bc1c2a3d8
Busza <78539366+Spokoziom123@users.noreply.github.com>
2022-11-28 17:07:30 +0000

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿//napisz program który podzieli dwie liczby podane od użytkownika
+try{
+    Console.WriteLine("Podaj x: ");
+    double x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Podaj y: ");
+    double y = int.Parse(Console.ReadLine());
+    if(y == 0) throw new DivideByZeroException("Nie dziel przez zero");
+    Console.WriteLine($"wynik dzielenia {x} przez {y} to {x/y}");
+}
+catch(FormatException e){
+    Console.WriteLine("Błąd konwersjii: "+e.Message);
+}
+catch(OutOfMemoryException e){
+    Console.WriteLine("Zbyt mało pamięci: "+e.Message);
+}
+catch(StackOverflowException e){
+    Console.WriteLine("Przepełnienie Stosu"+e.Message);
+}
+catch(ArithmeticException e){
+    Console.WriteLine("Błąd Arytmatyczny: " + e.Message);
+}
+catch(Exception e){
+    Console.WriteLine("Error: "+e.Message);
+}
+finally{
+    Console.WriteLine(":)");
+}
+ 
+
+
+
+ 
